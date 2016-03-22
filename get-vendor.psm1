f92d70df095f42a1765182f6759054368fcc6a0a
amolsen <anders@amolsen.com>
2016-03-22 13:05:52 +0100

    Uploaded Get-Vendor.psm1

---
+++ get-vendor.psm1
@@ -0,0 +1,29 @@
+﻿<# 
+
+ .Synopsis
+  Displays a Mac address vendor for given MAC address
+
+ .Description
+  Displays a visual representation of a calendar. This function supports multiple months
+  and lets you highlight specific date ranges or days.
+
+
+#>
+
+
+
+function get-vendor {
+    param(
+    [parameter(Mandatory=$true)]
+    $macaddr)
+
+    $uri = "http://www.coffer.com/mac_find/?string=$macaddr"
+
+            $macsearchresult = Invoke-WebRequest -Uri $uri
+
+            $vendor = ($macsearchresult.ParsedHtml.getElementsByTagName("td") | Where{ $_.className -eq ‘Table2’ }).innerText
+            Write-Output "MAC search for "
+
+            Write-Warning "$vendor"
+
+            }
