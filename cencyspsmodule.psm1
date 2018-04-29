a32b5ef055cde95f43449fe1eb700eb0549771a0
amolsen <anders@amolsen.com>
2018-04-29 20:41:45 +0200

    First Commit of Cencys PSTools

---
+++ cencyspsmodule.psm1
@@ -0,0 +1,37 @@
+﻿## censys basic module:
+$apiid = "ApiID";
+$secret = "ApiSecret"
+
+
+
+function get-csAuthHeader {
+    param([string]$myapiid = $apiid,
+        [string]$myapisecret = $apisecret)
+
+$pair = "${apiid}:${secret}"
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($pair); $base64 = [System.Convert]::ToBase64String($bytes)
+
+$basicAuthValue = "Basic $base64"; $headers = @{ Authorization = $basicAuthValue }
+
+return $headers
+}
+
+
+function new-csquery {
+    param([string]$csquery,
+           $headers=$headers)    
+
+
+    
+$qtojson = @{query=$csquery;
+             }
+
+$get = Invoke-RestMethod https://censys.io/api/v1/search/ipv4 -Headers $headers -Method Post -Body ($qtojson | ConvertTo-Json)
+
+
+return $get.results
+
+}
+
+
+
